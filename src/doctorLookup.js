28b1129b5dc3ec1b73a0d4a610b08e5bdd3ce711
Robert Lee <bobbydwise1@hotmail.com>
2019-01-18 10:05:05 -0800

    Debug API key issue.

--- src/doctorLookup.js
+++ src/doctorLookup.js
@@ -2,7 +2,7 @@ export class doctorLookup {
   getDoctorNames() {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = 'https://api.betterdoctor.com/2016-03-01/doctors?location=37.773,-122.413,100&skip=2&limit=10&user_key=${process.env.user_key}';
+      let url = 'https://api.betterdoctor.com/2016-03-01/doctors?location=37.773,-122.413,100&skip=2&limit=10&user_key=' + process.env.user_key;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
