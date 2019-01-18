c9f42b7fcdf31f75a1b40ca23b6737c8e425c9a2
Robert Lee <bobbydwise1@hotmail.com>
2019-01-18 12:53:24 -0800

    Debug API URL creation function.

--- src/doctorLookup.js
+++ src/doctorLookup.js
@@ -1,20 +1,20 @@
-export class doctorLookup {
-  createURLForDoc(fullname,symptom,geoLocation,radius,limit) {
-    let url = ('https://api.betterdoctor.com/2016-03-01/doctors?query=' +
+export class DoctorLookup {
+  createLink(fullname,symptom,geoLocation,radius,limit) {
+    return ('https://api.betterdoctor.com/2016-03-01/doctors?query=' +
     symptom +
     '&location=' +
     geoLocation.replace(",","%2C") +
     "%2C" + radius +
     "&sort=best-match-asc&skip=0&limit=" +
     limit +
-    '&user_key=')
-    return url;
+    '&user_key=');
   }
 
-  getDoctorNames() {
+  getDoctorNames(searchURL) {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = 'https://api.betterdoctor.com/2016-03-01/doctors?query=cancer&location=45.5122%2C-122.6587%2C5&user_location=45.5122%2C-122.6587&sort=best-match-asc&skip=0&limit=3&user_key=' + process.env.user_key;
+      let url = searchURL + process.env.user_key;
+      // let url = 'https://api.betterdoctor.com/2016-03-01/doctors?query=cancer&location=45.5122%2C-122.6587%2C5&user_location=45.5122%2C-122.6587&sort=best-match-asc&skip=0&limit=3&user_key=' + process.env.user_key;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
--- src/main.js
+++ src/main.js
@@ -2,18 +2,19 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import $ from 'jquery';
-import { doctorLookup } from './doctorLookup.js';
+import { DoctorLookup } from './doctorLookup.js';
 
 $(document).ready(function() {
-  let testurl = createURLForDoc("","diabetes","45.5122,-122.6587,",5,3)
-  console.log("testOfDocs api url: ",testurl)
 
-  let listOfDocs = new doctorLookup();
-  let promise = listOfDocs.getDoctorNames();
+  let listOfDocs = new DoctorLookup();
+  let testurl = listOfDocs.createLink("","diabetes","45.5122,-122.6587,",5,3)
+  console.log("testOfDocs api url: ",testurl)
+  let promise = listOfDocs.getDoctorNames(testurl);
 
   promise.then(function(response) {
     let body = JSON.parse(response);
     let doctor_array = body.data;
+    console.log(doctor_array);
     $("#doctorOutput").text(doctor_array)
   }, function(error) {
       $('.showErrors').text(`There was an error processing your request: ${error.message}`);
