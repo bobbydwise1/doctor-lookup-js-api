c30adcbd280ff969f7e1e686f78019daaaed033b
Robert Lee <bobbydwise1@hotmail.com>
2019-01-18 16:15:27 -0800

    Turn JSON output object into human readable HTML.

--- src/main.js
+++ src/main.js
@@ -14,21 +14,14 @@ $(document).ready(function() {
     let radius = 5;
     let limit = $("#searchInputNumResults").val();
     let testurl = listOfDocs.createLink(fullname,symptom,geolocation,radius,limit);
-    console.log("testOfDocs api url: ",testurl)
     let promise = listOfDocs.getDoctorNames(testurl);
 
     promise.then(function(response) {
       let body = JSON.parse(response);
       let doctor_array = body.data;
       for (let i = 0; i < doctor_array.length; i++) {
-        document.getElementById("doctorOutput").innerHTML =
-        '<p>Search Result: ${i}</p>
-        <p>${doctor_array[i].profile.first_name} ${doctor_array[i].profile.last_name}</p>
-        <p>Accepts new patients? ${doctor_array[i].practices[0].accepts_new_patients}</p>
-        <p>Website: " ${doctor_array[i].practices[0].website}</p>
-        <p>${doctor_array[i].practices[0].visit_address.street}</p>
-        <p>${doctor_array[i].practices[0].visit_address.city} ${doctor_array[i].practices[0].visit_address.state} ${doctor_array[i].practices[0].visit_address.zip}</p>
-        <p>${doctor_array[i].practices[0].phones}</p>'
+       $("#doctorOutput").append(`<p>Search Result: ${i}</p><p>${doctor_array[i].profile.first_name} ${doctor_array[i].profile.last_name}</p><p>Accepts new patients? ${doctor_array[i].practices[0].accepts_new_patients}</p><p>Website: " ${doctor_array[i].practices[0].website}</p><p>${doctor_array[i].practices[0].visit_address.street}</p>   <p>${doctor_array[i].practices[0].visit_address.city} ${doctor_array[i].practices[0].visit_address.state} ${doctor_array[i].practices[0].visit_address.zip}</p><p>${JSON.stringify(doctor_array[i].practices[0].phones)}</p>`)
+      }
     }, function(error) {
         $('.showErrors').text(`There was an error processing your request: ${error.message}`);
     });
