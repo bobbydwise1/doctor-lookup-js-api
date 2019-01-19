72827e78f336a0781c28a2079a6a76c211d0517a
Robert Lee <bobbydwise1@hotmail.com>
2019-01-18 16:38:07 -0800

    Finalize Bussiness and UI logic.

--- src/main.js
+++ src/main.js
@@ -20,9 +20,13 @@ $(document).ready(function() {
     promise.then(function(response) {
       let body = JSON.parse(response);
       let doctor_array = body.data;
-      for (let i = 0; i < doctor_array.length; i++) {
+      $("#doctorOutput").empty();
+      if (doctor_array.length === 0)
+      {$("#doctorOutput").append(`<div class="resultBox"><p>No results found</p></div>`)}
+      else 
+      {for (let i = 0; i < doctor_array.length; i++) {
        $("#doctorOutput").append(`<div class="resultBox"><p>Search Result: ${i}</p><h5>${doctor_array[i].profile.first_name} ${doctor_array[i].profile.last_name}</h5><p>Accepts new patients? ${doctor_array[i].practices[0].accepts_new_patients}</p><p>Website: " ${doctor_array[i].practices[0].website}</p><p>${doctor_array[i].practices[0].visit_address.street}</p>   <p>${doctor_array[i].practices[0].visit_address.city} ${doctor_array[i].practices[0].visit_address.state} ${doctor_array[i].practices[0].visit_address.zip}</p><p>${JSON.stringify(doctor_array[i].practices[0].phones)}</p></div>`)
-      }
+      }}
     }, function(error) {
         $('.showErrors').text(`There was an error processing your request: ${error.message}`);
     });
